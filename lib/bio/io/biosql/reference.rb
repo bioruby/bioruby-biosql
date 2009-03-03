c132e9e3d33579c0f02379e86c194cefcaa5278b
Ra <ilpuccio.febo@gmail.com>
2009-03-03 19:25:12 +0100

    Temporal removal of BioSQL ActiveRecords classes (being restored soon).
    To integrate BioSQL ActiveRecords classes to one file, as the first
    step, files listed below are deleted. In the later commit, they will
    be integrated into one file, lib/bio/io/biosql/ar-biosql.rb.
    
    This is a combination of 28 commits made by Raoul Jean Pierre Bonnal.
    
    Deleted files:
     * lib/bio/io/biosql/biodatabase.rb
     * lib/bio/io/biosql/bioentry.rb
     * lib/bio/io/biosql/bioentry_dbxref.rb
     * lib/bio/io/biosql/bioentry_path.rb
     * lib/bio/io/biosql/bioentry_qualifier_value.rb
     * lib/bio/io/biosql/bioentry_reference.rb
     * lib/bio/io/biosql/bioentry_relationship.rb
     * lib/bio/io/biosql/biosequence.rb
     * lib/bio/io/biosql/comment.rb
     * lib/bio/io/biosql/dbxref.rb
     * lib/bio/io/biosql/dbxref_qualifier_value.rb
     * lib/bio/io/biosql/location.rb
     * lib/bio/io/biosql/location_qualifier_value.rb
     * lib/bio/io/biosql/ontology.rb
     * lib/bio/io/biosql/reference.rb
     * lib/bio/io/biosql/seqfeature.rb
     * lib/bio/io/biosql/seqfeature_dbxref.rb
     * lib/bio/io/biosql/seqfeature_path.rb
     * lib/bio/io/biosql/seqfeature_qualifier_value.rb
     * lib/bio/io/biosql/seqfeature_relationship.rb
     * lib/bio/io/biosql/taxon.rb
     * lib/bio/io/biosql/taxon_name.rb
     * lib/bio/io/biosql/term.rb
     * lib/bio/io/biosql/term_dbxref.rb
     * lib/bio/io/biosql/term_path.rb
     * lib/bio/io/biosql/term_relationship.rb
     * lib/bio/io/biosql/term_relationship_term.rb
     * lib/bio/io/biosql/term_synonym.rb

--- lib/bio/io/biosql/reference.rb
+++
@@ -1,9 +0,0 @@
-
- module Bio
-    class SQL 
-		    class Reference < DummyBase  
-		      belongs_to :dbxref
-		      has_many :bioentry_references, :class_name=>"BioentryRefernce"
-		    end		
-		end #SQL
-end #Bio
--- lib/bio/io/biosql/taxon_name.rb
+++
@@ -1,9 +0,0 @@
-
- module Bio
-    class SQL 
-			    class TaxonName < DummyBase
-			      set_primary_keys :taxon_id, :name, :name_class
-			      belongs_to :taxon            
-			    end		
-		end #SQL
-end #Bio
